9824c0f716ffc8ec3e7029907a557f669ce0b84a
Adarsh gupta <companybyadarsh5435@gmail.com>
2025-10-17 13:59:12 +0530

    updated contact js

--- contact.js
+++ contact.js
@@ -16,10 +16,30 @@ if (contactForm) {
     this.reset(); 
   });
 }
-// Dark mode toggle
+// Dark mode 
 const toggle = document.getElementById("darkModeToggle");
 if (toggle) {
   toggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
   });
 }
+
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const response = await fetch(form.action, {
+    method: form.method,
+    body: formData,
+    headers: { Accept: "application/json" }
+  });
+
+  if (response.ok) {
+    alert("Your message has been sent successfully!");
+    form.reset();
+  } else {
+    alert("Oops! Something went wrong. Please try again later.");
+  }
+});
